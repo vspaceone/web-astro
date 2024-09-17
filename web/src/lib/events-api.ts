f44fa8bf7c8f6c64a32ac036c3c405de69c4ec57
enricobuehler <buehler@unom.io>
2024-09-17 20:20:02 +0200

    update deps

--- web/src/lib/events-api.ts
+++ web/src/lib/events-api.ts
@@ -1,7 +1,7 @@
 import ICal from "ical";
 
 const CALENDAR_ICAL_URL =
-    "https://hub.vspace.one/remote.php/dav/public-calendars/f6MfGLnsGScRqd4Y?export";
+	"https://hub.vspace.one/remote.php/dav/public-calendars/f6MfGLnsGScRqd4Y?export";
 //const CALENDAR_URL = "https://hub.vspace.one/apps/calendar/p/f6MfGLnsGScRqd4Y";
 
 // Only look at this much next occurences, to prevent infinite loop
@@ -11,130 +11,126 @@ const DESIGNATOR_LINK = "Link";
 const DESIGNATOR_DOWNLOAD = "Download";
 
 export async function getCalenderIcs() {
-    const response = await fetch(CALENDAR_ICAL_URL);
-    const data = await response.text();
+	const response = await fetch(CALENDAR_ICAL_URL);
+	const data = await response.text();
 
-    return data;
+	return data;
 }
 
 export type Event = {
-    title: string;
-    start: Date;
-    end: Date;
-    location: string;
-    description?: string;
-    link: string | undefined;
-    download: string | undefined;
-    isRecurring: boolean;
+	title: string;
+	start: Date | undefined;
+	end: Date | undefined;
+	location: string;
+	description?: string;
+	link: string | undefined;
+	download: string | undefined;
+	isRecurring: boolean;
 };
 
 export async function getEvents() {
-    // Fetch the calender ics
-    const icsData = await getCalenderIcs();
+	// Fetch the calender ics
+	const icsData = await getCalenderIcs();
 
-    const events = ICal.parseICS(icsData);
+	const events = ICal.parseICS(icsData);
 
-    // Filters are split up for readability
-    const futureAndRecurringEvents = Object.values(events)
-        // Only include events of type VEVENT
-        .filter((e) => e.type === "VEVENT")
-        // Exclude Past events that aren't recurring
-        .filter((e) => {
-            const isFuture = Number(e.end) > Date.now();
-            const isRecurring = !!e.rrule;
+	// Filters are split up for readability
+	const futureAndRecurringEvents = Object.values(events)
+		// Only include events of type VEVENT
+		.filter((e) => e.type === "VEVENT")
+		// Exclude Past events that aren't recurring
+		.filter((e) => {
+			const isFuture = Number(e.end) > Date.now();
+			const isRecurring = !!e.rrule;
 
-            return isFuture || isRecurring;
-        })
-        // Get latest reccurance of reccuring events
-        .map((e) => {
-            const isRecurring = !!e.rrule;
+			return isFuture || isRecurring;
+		})
+		// Get latest reccurance of reccuring events
+		.map((e) => {
+			const isRecurring = !!e.rrule;
 
-            if (!isRecurring || !e.rrule) return e;
+			if (!isRecurring || !e.rrule) return e;
 
-            else {
+			const latest = e.recurrences?.[e.recurrences.length - 1];
 
-                const latest = e.recurrences?.[e.recurrences.length - 1];
+			if (!latest) {
+				const duration =
+					e.end !== undefined && e.start !== undefined
+						? e.end.getTime() - e.start.getTime()
+						: 0;
+				const next = e.rrule.after(new Date(Date.now()), true);
 
-                if (!latest) {
-                    const duration = e.end !== undefined && e.start !== undefined ? e.end.getTime() - e.start.getTime() : 0;
-                    const next = e.rrule.after(new Date(Date.now()), true);
+				if (!next) {
+					return e;
+				}
 
-                    if (!next) {
-                        return e;
-                    }
+				e.start = next;
+				e.end = new Date(next.getTime() + duration);
 
-                    e.start = next;
-                    e.end = new Date(next.getTime() + duration);
+				return e;
+			}
+			return latest;
+		})
+		.filter((e) => {
+			const isFuture = Number(e.end) > Date.now();
 
-                    return e
+			return isFuture;
+		})
+		// Exclude cancelled events
+		.filter((e) => {
+			const isCancelled = e.status !== undefined && e.status === "CANCELLED";
 
-                } else {
-                    return latest
-                }
-            }
-        })
-        .filter((e) => {
-            const isFuture = Number(e.end) > Date.now();
+			// Only return events that aren't cancelled
+			return !isCancelled;
+		})
+		// Get designator values from descriptions and turn into event type
+		.map((e) => {
+			let download: string | undefined = undefined;
+			let link: string | undefined = undefined;
+			let description = "";
 
-            return isFuture;
-        })
-        // Exclude cancelled events
-        .filter((e) => {
-            const isCancelled = e.status !== undefined && e.status === "CANCELLED";
+			const isRecurring = !!e.rrule;
 
-            // Only return events that aren't cancelled
-            return !isCancelled;
-        })
-        // Get designator values from descriptions and turn into event type
-        .map((e) => {
-            let download: string | undefined = undefined;
-            let link: string | undefined = undefined;
-            let description = "";
+			if (e.description) {
+				const lines = e.description.split("\n");
 
-            const isRecurring = !!e.rrule;
+				for (const line of lines) {
+					const designator = line.substring(0, line.search(":"));
 
-            //  console.log(e.description);
+					switch (designator) {
+						case DESIGNATOR_DOWNLOAD:
+							download = line
+								.substring(DESIGNATOR_DOWNLOAD.length + 1, line.length)
+								.trim();
+							break;
+						case DESIGNATOR_LINK:
+							link = line
+								.substring(DESIGNATOR_LINK.length + 1, line.length)
+								.trim();
+							break;
+						default:
+							description += `${line} `;
+					}
+				}
+			}
 
-            if (e.description) {
-                const lines = e.description.split("\n");
+			description = description.trim();
 
-                for (let line of lines) {
-                    const designator = line.substring(0, line.search(":"));
+			const event: Event = {
+				title: e.summary || "Kein Titel",
+				start: e.start,
+				end: e.end,
+				location: e.location || "Kein Standort",
+				description,
+				link,
+				download,
+				isRecurring,
+			};
 
-                    switch (designator) {
-                        case DESIGNATOR_DOWNLOAD:
-                            download = line
-                                .substring(DESIGNATOR_DOWNLOAD.length + 1, line.length)
-                                .trim();
-                            break;
-                        case DESIGNATOR_LINK:
-                            link = line
-                                .substring(DESIGNATOR_LINK.length + 1, line.length)
-                                .trim();
-                            break;
-                        default:
-                            description += line + " ";
-                    }
-                }
-            }
+			return event;
+		})
+		// Sort the events
+		.sort((a, b) => (a.start?.getTime() || 0) - (b.start?.getTime() || 0));
 
-            description = description.trim();
-
-            const event: Event = {
-                title: e.summary || "Kein Titel",
-                start: e.start!,
-                end: e.end!,
-                location: e.location || "Kein Standort",
-                description,
-                link,
-                download,
-                isRecurring,
-            };
-
-            return event;
-        })
-        // Sort the events
-        .sort((a, b) => a.start.getTime() - b.start.getTime());
-
-    return futureAndRecurringEvents;
+	return futureAndRecurringEvents;
 }
